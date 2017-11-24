336476e73f62e0e0040a9752a6948ab5de0511c0
charlesmccrary <charles.mccrary@gmail.com>
2017-11-23 21:05:29 -0500

    Initial Commit

---
+++ TestingCpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TestingCpp
+//
+//  Created by Charles McCrary on 11/23/17.
+//  Copyright © 2017 Charles McCrary. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
